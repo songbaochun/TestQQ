c8e0f4550a1cf120b71e3dae798e19053d37a9d1
unknown <>
2017-03-22 14:03:31 +0800

    --mbase Activity

---
+++ app/src/main/java/com/example/testqq/activity/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.example.testqq.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 宋宝春 on 2017/3/22.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
